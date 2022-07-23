bb404990673a3c40782a7cb4f6da9c2cb7a8d99f
amit.sahu <amit.sahu@codenicely.in>
2022-07-23 05:38:36 +0530

    update the tech stack section minor toches in navbar our doain section finished

--- src/components/landig-page/OurDomain.vue
+++ src/components/landig-page/OurDomain.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="py-15 app-padding our-domain-section">
-    <v-row align="center" justify="center" class="mb-10" no-gutters>
+    <v-row align="center" justify="center" class="mb-8 my-5" no-gutters>
       <v-col sm="12" cols="13" class="justify-center text-center">
         <p class="font-weight-bold text-primary px-10" :class="$vuetify.display.mobile?'text-h5 ':'text-h4'">
           Have a idea in mind? We can help you build it !
         </p>
-        <p class="mt-4 text-black" :class="$vuetify.display.mobile?'text-subtitle-1 ':' text-h6'">
+        <p class="mt-5 text-black" :class="$vuetify.display.mobile?'text-subtitle-1 ':' text-subtitle-1'">
           We design products that cater to your brand and scale your business
           through increased productivity and optimal performance.We can help
           you, no matter what is your domain or how big or small your business is!
